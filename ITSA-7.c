9e456dc6e59f9afb72480c474e9e818c38712d17
YuPatty <taes10056@gmail.com>
2024-09-18 12:04:56 +0800

    7_AC

--- ITSA-7.c
+++ ITSA-7.c
@@ -9,7 +9,7 @@ int main(){
     complex c1, c2, result;
     scanf("%d", &n);
     for(int i = 0; i < n; i++){
-        scanf(" %c %d %d %d %d", &op, &c1.real, &c1.imag, &c2.real, &c2.imag);
+        scanf(" %c %d %d %d %d", &op, &c1.real, &c1.imag, &c2.real, &c2.imag);  //留一個空格以避免換行符被錯誤讀取
 
         if(op == '+'){
             result.real = c1.real + c2.real;
@@ -22,6 +22,14 @@ int main(){
         else if(op == '*'){
             result.real = c1.real * c2.real - c1.imag * c2.imag;
             result.imag = c1.real * c2.imag + c1.imag * c2.real;
+        }
+        else if(op == '/'){
+            float denominator = c2.real * c2.real + c2.imag * c2.imag;
+            if (denominator == 0) {
+                continue;
+            }
+            result.real = (c1.real * c2.real + c1.imag * c2.imag) / denominator;
+            result.imag = (c1.imag * c2.real - c1.real * c2.imag) / denominator;
         }
         printf("%d %d\n", result.real, result.imag);
     }
